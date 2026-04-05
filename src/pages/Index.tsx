50b0d7970d79f35633546dea4e77ea1243b34cc5
Alexander Kazanski <alexander1.15.1943@gmail.com>
2026-04-05 08:20:14 -0400

    chore: update date

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -67,7 +67,7 @@ const metrics: MetricCardProps[][] = [
         { date: 'March 11, 2026', icon: Thermometer, label: "Temperature", value: "98.1°F" },
     ]
 
-][1];
+][2];
 
 const Index = () => (
     <div className="min-h-screen bg-background">
